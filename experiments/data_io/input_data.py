d67ab8b6a7379ba178c84b3ca8ad61374faff2d2
Ken Wong <clwong@us.ibm.com>
2024-04-27 21:43:38 -0700

    Modified to use PyDataset.

--- experiments/data_io/input_data.py
+++ experiments/data_io/input_data.py
@@ -5,8 +5,10 @@
 
 import math
 
-from .datagenerator import DataGenerator
-from tensorflow.keras.utils import Sequence, OrderedEnqueuer
+from keras.src.trainers.data_adapters.py_dataset_adapter import PyDatasetAdapter
+
+from .dataset import MultimodalImageDataset, ImageTransform
+
 
 __author__ = 'Ken C. L. Wong'
 
@@ -34,9 +36,8 @@ class InputData(object):
         workers: Number of parallel processes (default: 1).
             If 0, no threading or multiprocessing is used.
         use_multiprocessing: Use multiprocessing if True, otherwise threading (default: False).
-        generator_kwargs: Dict of keyword arguments for DataGenerator (default: None).
-            E.g., arguments for data augmentation.
-            See DataGenerator for details.
+        transform_kwargs: Dict of keyword arguments for ImageTransform (default: None).
+            See `.dataset.ImageTransform` for details.
     """
     def __init__(self,
                  reader=None,
@@ -50,9 +51,9 @@ class InputData(object):
                  max_queue_size=1,
                  workers=1,
                  use_multiprocessing=False,
-                 generator_kwargs=None,
+                 transform_kwargs=None,
                  ):
-        self.reader = reader
+        self.reader = reader or (lambda x: x)
         self.data_lists_train = data_lists_train
         self.data_lists_valid = data_lists_valid
         self.data_lists_test = data_lists_test
@@ -63,74 +64,65 @@ class InputData(object):
         self.max_queue_size = max_queue_size
         self.workers = workers
         self.use_multiprocessing = use_multiprocessing
-        self.generator_kwargs = generator_kwargs
+        self.transform_kwargs = transform_kwargs
 
-        self.train_enqueuer = None
-        self.valid_enqueuer = None
-        self.test_enqueuer = None
-
-    def _get_flow(self, data_lists, shuffle=False, generator_kwargs=None, seed=None):
-        generator_kwargs = generator_kwargs or {}
-        generator = DataGenerator(**generator_kwargs).flow(
+    def _get_flow(self, data_lists, shuffle=False, transform_kwargs=None):
+        transform = ImageTransform(**transform_kwargs) if transform_kwargs is not None else None
+        dataset = MultimodalImageDataset(
+            data_lists,
+            self.batch_size,
             reader=self.reader,
-            data_lists=data_lists,
             idx_x_modalities=self.idx_x_modalities,
             idx_y_modalities=self.idx_y_modalities,
             x_processing=self.x_processing,
-            batch_size=self.batch_size, shuffle=shuffle, seed=seed)
+            transform=transform,
+            workers=self.workers,
+            use_multiprocessing=self.use_multiprocessing,
+            max_queue_size=self.max_queue_size,
+        )
 
-        generator, enqueuer = get_threaded_generator(generator, max_queue_size=self.max_queue_size,
-                                                     workers=self.workers,
-                                                     use_multiprocessing=self.use_multiprocessing)
-        return generator, enqueuer
+        adapter = PyDatasetAdapter(
+            dataset,
+            shuffle=shuffle
+        )
 
-    def get_train_flow(self, shuffle=True, seed=None):
-        """Gets the generator for training.
+        return adapter
+
+    def get_train_flow(self, shuffle=True):
+        """Gets the PyDatasetAdapter for training.
 
         Args:
             shuffle: If True (default), the data are shuffled in each epoch.
-            seed: For controlling the randomness (default: None).
-                Warning: may not work as expected.
 
         Returns:
-            A generator that loops indefinitely.
+            A PyDatasetAdapter that can create an iterator for one epoch by get_numpy_iterator().
         """
-        generator, self.train_enqueuer = self._get_flow(
-            self.data_lists_train, shuffle=shuffle, generator_kwargs=self.generator_kwargs,
-            seed=seed
-        )
-        return generator
+        return self._get_flow(self.data_lists_train, shuffle=shuffle, transform_kwargs=self.transform_kwargs)
 
     def get_valid_flow(self):
-        """Gets the generator for validation.
+        """Gets the PyDatasetAdapter for validation.
         No data shuffling and augmentation.
 
         Returns:
-            A generator that loops indefinitely.
+            A PyDatasetAdapter that can create an iterator for one epoch by get_numpy_iterator().
         """
-        generator, self.valid_enqueuer = self._get_flow(
-            self.data_lists_valid, shuffle=False, generator_kwargs=None, seed=None
-        )
-        return generator
+        return self._get_flow(self.data_lists_valid)
 
     def get_test_flow(self):
-        """Gets the generator for testing.
+        """Gets the PyDatasetAdapter for testing.
         No data shuffling and augmentation.
 
         Returns:
-            A generator that loops indefinitely.
+            A PyDatasetAdapter that can create an iterator for one epoch by get_numpy_iterator().
         """
-        generator, self.test_enqueuer = self._get_flow(
-            self.data_lists_test, shuffle=False, generator_kwargs=None, seed=None
-        )
-        return generator
+        return self._get_flow(self.data_lists_test)
 
     def _get_num_batches(self, data):
         if data is None:
             return 0
 
         num_samples = len(data[0])
-        return int(math.ceil(num_samples / self.batch_size))
+        return math.ceil(num_samples / self.batch_size)
 
     def get_train_num_batches(self):
         data = self.data_lists_train
@@ -157,42 +149,3 @@ class InputData(object):
 
     def get_test_image_size(self):
         return self._get_image_size(self.data_lists_test)
-
-    def stop_enqueuers(self):
-        """Stops running threads if necessary."""
-        if self.train_enqueuer is not None:
-            self.train_enqueuer.stop()
-            self.train_enqueuer = None
-        if self.valid_enqueuer is not None:
-            self.valid_enqueuer.stop()
-            self.valid_enqueuer = None
-        if self.test_enqueuer is not None:
-            self.test_enqueuer.stop()
-            self.test_enqueuer = None
-
-
-def get_threaded_generator(generator, max_queue_size=1, workers=1, use_multiprocessing=False):
-    """Gets a threaded generator which runs in parallel with other processes.
-
-    Args:
-        generator: A generator.
-        max_queue_size: Maximum queue size (default: 1).
-        workers: Number of parallel processes (default: 1).
-            If 0, no threading or multiprocessing is used.
-        use_multiprocessing: Use multiprocessing if True, otherwise threading (default: False).
-
-    Returns:
-        A threaded generator and an enqueuer which controls the thread.
-    """
-    if not isinstance(generator, Sequence):
-        raise ValueError('Must use a generator inherited from the keras.utils.Sequence class.')
-
-    enqueuer = None
-    if workers > 0:
-        enqueuer = OrderedEnqueuer(generator, use_multiprocessing=use_multiprocessing)
-        enqueuer.start(workers=workers, max_queue_size=max_queue_size)
-        output_generator = enqueuer.get()
-    else:
-        output_generator = iter(generator)
-
-    return output_generator, enqueuer
